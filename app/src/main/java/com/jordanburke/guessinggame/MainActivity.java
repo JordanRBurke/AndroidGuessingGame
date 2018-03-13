429b751e0900688c8c2141dbc6aa2203259f71cc
JordanRBurke <jordanrobertburke@gmail.com>
2018-03-13 16:30:10 -0400

    Finished both activity functions. Started working on logic for guessing

--- app/src/main/java/com/jordanburke/guessinggame/MainActivity.java
+++ app/src/main/java/com/jordanburke/guessinggame/MainActivity.java
@@ -2,13 +2,49 @@ package com.jordanburke.guessinggame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button startButton;
+    private Button submitButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startButton = findViewById(R.id.start_button);
+        submitButton = findViewById(R.id.start_button);
+        sendMessage();
+
+    }
+
+    public void sendMessage() {
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//
+                Intent thisIntent = new Intent(MainActivity.this, GuessPageActivity.class);
+//                thisIntent.putExtra("key", value); //Optional parameters
+                MainActivity.this.startActivity(thisIntent);
+            }
+        }
+        );
+    }
+    public void submitButton() {
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
 
 
     }
