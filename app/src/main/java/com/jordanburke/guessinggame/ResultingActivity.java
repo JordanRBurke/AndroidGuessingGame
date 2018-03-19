294627871ca439e50541d2c76a25a602c4c72182
JordanRBurke <jordanrobertburke@gmail.com>
2018-03-19 13:19:46 -0400

    Added sad face and happy face into drawable file and places happy face in Resulting activity

--- app/src/main/java/com/jordanburke/guessinggame/ResultingActivity.java
+++ app/src/main/java/com/jordanburke/guessinggame/ResultingActivity.java
@@ -30,7 +30,7 @@ public class ResultingActivity extends AppCompatActivity {
         playAgainButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+
             }
         });
     }
